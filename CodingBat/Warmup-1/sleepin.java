0a48edb224ca9b433cc9a39a22d3cfe781914682
nathanhoehndorf <hoehndorf.nathan@gmail.com>
2023-10-18 16:43:05 -0600

    completed sleepin.java

--- CodingBat/Warmup-1/sleepin.java
+++ CodingBat/Warmup-1/sleepin.java
@@ -1,14 +1,24 @@
+/**
+ * 
+ * The parameter weekday is true if it is a weekday, and the parameter vacation 
+ * is true if we are on vacation. We sleep in if it is not a weekday or we're on 
+ * vacation. Return true if we sleep in.
+ * 
+ */
 class sleepin {
 
-    public boolean sleepIn(boolean weekday, boolean vacation) {
+    static boolean sleepIn(boolean weekday, boolean vacation) {
   
-  
+        return vacation || !weekday;
   
     }
 
     public static void main(String[] args) {
 
-        
+        System.out.println(sleepIn(false, false));
+        System.out.println(sleepIn(true, false));
+        System.out.println(sleepIn(false, true));
+        System.out.println(sleepIn(true, true));
 
     }
 
